3df1155a43872f476323cd20a959aa9d74a19b90
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-01 21:42:42 +0700

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Truong Nguyen on 9/1/20.
+//  Copyright © 2020 Truong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
