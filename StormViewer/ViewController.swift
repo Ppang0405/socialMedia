c67089faa65118e45965055b5bd16562bc53c726
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-01 22:38:05 +0700

    👻 update ViewController by add comment

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,17 +17,28 @@ class ViewController: UIViewController {
         let fm = FileManager.default
         print(fm, "fm")
         
-        let path = Bundle.main.resourcePath!
+        // https://www.hackingwithswift.com/example-code/system/how-to-find-the-path-to-a-file-in-your-bundle
+        // It’s common to store resource data like text files and sound effects inside your bundle
+        
+        // Bundle.main: Returns the bundle object that contains the current executable.
+        
+        let path = Bundle.main.resourcePath! // ios app alwayshas resourcePath, but use ! for others
         print(path, "path")
         
-        let items = try! fm.contentsOfDirectory(atPath: path)
+        let items = try! fm.contentsOfDirectory(atPath: path) // force try, can crash if can not try
         print("items", items)
+        
+        print("pictures", pictures)
         
         for item in items {
             if item.hasPrefix("nssl") {
                 print(item)
+                
+                pictures.append(item)
             }
         }
+        
+        print("pictures_2", pictures)
     }
 
 
